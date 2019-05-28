7cc1e8b612ed5e3de37c041c1400647992bb681d
Constanza Díaz <constanza.diaz@gmail.com>
2019-05-28 11:56:54 +0200

    trying beforestartprompt

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,7 @@ self.addEventListener('install', function(event) {
  event.waitUntil(
    caches.open('github-users').then(function(cache) {
      return cache.addAll([
-       '/',
        '/index.html',
-       '/index.html?homescreen=1',
-       '/?homescreen=1',
        '/styles/index.css',
        '/index.js',
      ]);
